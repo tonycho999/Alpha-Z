aaf9b68eaf459a8b2048e5a6ea38179f6f57330f
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-18 15:42:05 +0800

    Delete js directory

--- js/levels.js
+++
@@ -1,6 +0,0 @@
-const LEVELS = [
-    { id: 1, target: "GO", size: 4 },   // 레벨 1: 4x4, 목표 GO
-    { id: 2, target: "CAT", size: 4 },  // 레벨 2: 4x4, 목표 CAT
-    { id: 3, target: "LOVE", size: 4 }, // 레벨 3: 4x4, 목표 LOVE
-    { id: 4, target: "BRAVE", size: 5 } // 레벨 4: 5x5, 목표 BRAVE
-];
