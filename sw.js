e8a671f01c3410c547ebc07afc0198b3b706493c
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-20 11:20:42 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
 
+const CACHE_NAME = 'alpha-z-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/game.html',
+  '/style.css',
+  '/js/index.js',
+  '/js/game-main.js',
+  '/js/game-core.js',
+  '/js/game-data.js',
+  '/js/game-ui.js',
+  '/js/shop.js',
+  '/manifest.json',
+  '/favicon.png',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
